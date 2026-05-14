d6972902daa2f6267e1d727a78ff214afa6512fd
Tungston-labs <jesna@tungstonlabs.com>
2026-05-14 11:44:36 +0530

    tobar pull request

--- src/pages/Proposition/index.jsx
+++ src/pages/Proposition/index.jsx
@@ -1,32 +1,40 @@
 import React, { useEffect, useState } from "react";
 import FranchiseBanner from "../../components/TopBanner";
 import franchiseImg from "../../../public/images/franchise.svg";
+import CompetitiveAdvantage from "../../components/Proposition/CompetitiveAdvantage";
 
 const Propositions = () => {
-  const titles = [
-    "Partner With A Global Franchise Leader",
-    "Expand Your Business Growth",
-    "Join Our Franchise Network",
-  ];
+    const titles = [
+        "Partner With A Global Franchise Leader",
+        "Turn Ambition Into International Success",
+        " Scale Your Business With a Proven Global Model",
+        "Expanding Opportunities Delivering Global Success",
+    ];
+    const [titleIndex, setTitleIndex] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTitleIndex((prev) => (prev + 1) % titles.length);
+        }, 3000);
 
-  const [titleIndex, setTitleIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTitleIndex((prev) => (prev + 1) % titles.length);
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, [titles.length]);
-
-  return (
-    <FranchiseBanner
-      key={titleIndex}
-      title={titles[titleIndex]}
-      description="Yet With Careful Planning, Focus, A Solid Chicking Network, And The Right Training And Support, You Can Position Your Business For Growth And Success."
-      image={franchiseImg}
-    />
-  );
+        return () => clearInterval(interval);
+    }, [titles.length]);
+    return (
+        <>
+        <FranchiseBanner
+            key={titleIndex}
+            title={titles[titleIndex]}
+            description={
+                <>
+                    Yet with careful planning, focus, a solid Chicking network, and the right
+                    <br />
+                    training and support, you can position your business for growth and success.
+                </>
+            }
+            image={franchiseImg}
+        />
+        <CompetitiveAdvantage/>
+        </>
+    );
 };
 
 export default Propositions;
